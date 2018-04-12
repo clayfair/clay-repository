e7e3c8d6d412436735d46e49368b0fd367c191cb
Clay Fairbanks <clayafairbanks@gmail.com>
2018-04-12 18:22:54 -0400

    need about page

--- js/new-portfolio.js
+++ js/new-portfolio.js
@@ -4,3 +4,10 @@ $(".scroll").click(function() {
         scrollTop: $("#container-two").offset().top},
         'slow');
 });
+
+$(".up-scroll").click(function() {
+    console.log("hello");
+    $('html,body').animate({
+        scrollTop: $("#container-one").offset().top},
+        'slow');
+});
